d09457773238a2ae3c66ef3d51a857c0fcb43365
F53 <fseusb@gmail.com>
2022-12-23 13:56:34 -0700

    finalize tooltip and bump version

--- src/main/java/net/cardinalboats/CardinalBoatsInit.java
+++ src/main/java/net/cardinalboats/CardinalBoatsInit.java
@@ -1,14 +1,9 @@
 package net.cardinalboats;
 
-import net.cardinalboats.TurnPriming;
 import net.cardinalboats.config.ModConfig;
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CardinalBoatsInit implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
 	public static boolean LieAboutMovingForward = false;
 
 	@Override
